b389989dfbec93b4277c27addb4748fa6fb5373a
Hadil <hadilammar707@gmail.com>
2021-09-05 22:48:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+  
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { UserList } from './UserList';
 
 function App() {
+
+  const [listOFUsers, setlistOFUsers] = useState([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(function(response){
+      setlistOFUsers(response.data)
+    })
+
+    .catch(function (error) {
+      setError(error)
+    })
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+        <img width="100px" src="https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/512x512/users5.png" alt="users" />
+        <h2>The Users are :</h2>
+      </div>
+      <UserList users={listOFUsers}/>
     </div>
   );
 }
